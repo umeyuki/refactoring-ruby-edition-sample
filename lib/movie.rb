14d8c402e958c1aeeedae5b4f3ac5a4bc6308795
umeyuki <umeyuki1326+github@gmail.com>
2015-01-03 20:25:42 +0900

    他のオブジェクトの属性に基づいてswitch文を書くのは間違っているので、chargeメソッドをMovieクラスに移す

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Movie
   REGULAR     = 0
   NEW_RELEASE = 1
@@ -9,4 +10,20 @@ class Movie
   def initialize(title, price_code)
     @title, @price_code = title, price_code    
   end
+
+  def charge(days_rented)
+    result = 0
+    # 各行の金額を計算
+    case price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2)
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+  end
+  
 end
