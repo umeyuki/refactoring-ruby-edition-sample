81a78a7f6b957fcf795dbd37d3540b554ec852eb
umeyuki <umeyuki1326+github@gmail.com>
2015-01-03 16:43:08 +0900

    レンタルポイント計算メソッドの抽出

--- lib/rental.rb
+++ lib/rental.rb
@@ -19,5 +19,16 @@ class Rental
       result += (days_rented - 3) * 1.5 if days_rented > 3
     end
   end
+
+  def frequent_renter_points
+    # レンタルポイントを加算
+    result = 0
+    result += 1
+    # 新作2日間レンタルでボーナスポイントを加算
+    if movie.price_code == Movie::NEW_RELEASE && days_rented > 1
+      result += 1
+    end
+    result
+  end
   
 end
